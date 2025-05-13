fb552452d184f452e4173c680192484497f1acac
Daniel Lovera <daanilo2497@gmail.com>
2025-05-13 00:59:14 -0300

    refactor: update ramp geometry to use new version of parametric swept

--- src/geometry/road_ramp_geometry.ts
+++ src/geometry/road_ramp_geometry.ts
@@ -1,4 +1,3 @@
-import { Matrix4, Vector3 } from "three";
 import { CatmullRom } from "../utils/catmull_rom";
 import { linear } from "../utils/functions";
 
@@ -33,41 +32,26 @@ export class RoadRampGeometry {
     }
 
     private createParametricGeometry(width: number, height: number, curve: CatmullRom) {
-        const { segments, tangents, normals, binormals } = curve.computeFrenetFrames();
-        const geometry = curve.parametricSwept((u: number, v: number, target: Vector3) => {
-            const [x, y, z] = this.getRectanglePoint(u, v, width, height);
-            const point = new Vector3(x, y, z);
+        const mappingFn = (u: number, v: number): [number, number, number] => {
+            const [x, y] = (() => {
+                const side = u * RoadRampGeometry.PROFILE_SIDES;
+                const scaleFn = linear(height);
+                height = this.reverse ? scaleFn(v) : scaleFn(1 - v);
 
-            const index = Math.floor(v * segments);
-            const tangent = tangents[index];
-            const normal = normals[index];
-            const binormal = binormals[index];
-            const curvePoint = curve.getPointAt(v);
-            const sweptMatrix = new Matrix4().makeBasis(normal, binormal, tangent);
-            sweptMatrix.setPosition(curvePoint);
-            point.applyMatrix4(sweptMatrix);
-            target.set(point.x, point.y, point.z);
-        }, RoadRampGeometry.PROFILE_SIDES);
+                if (side < 1) {
+                    return [-height * side, -width / 2];
+                } else if (side < 2) {
+                    return [-height, -width / 2 + width * (side - 1)];
+                } else if (side < 3) {
+                    return [-height + height * (side - 2), width / 2];
+                } else {
+                    return [0, width / 2 - width * (side - 3)];
+                }
+            })();
+            return [x, y, 0];
+        };
 
+        const geometry = curve.parametricSwept(mappingFn, RoadRampGeometry.PROFILE_SIDES);
         return geometry;
-    }
-
-    private getRectanglePoint(u: number, v: number, width: number, height: number) {
-        const [x, y] = (() => {
-            const side = u * RoadRampGeometry.PROFILE_SIDES;
-            const scaleFn = linear(height);
-            height = this.reverse ? scaleFn(v) : scaleFn(1 - v);
-
-            if (side < 1) {
-                return [-height * side, -width / 2];
-            } else if (side < 2) {
-                return [-height, -width / 2 + width * (side - 1)];
-            } else if (side < 3) {
-                return [-height + height * (side - 2), width / 2];
-            } else {
-                return [0, width / 2 - width * (side - 3)];
-            }
-        })();
-        return [x, y, 0];
     }
 }
