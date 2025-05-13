4aa69f0b908a1154f3b7c0c8941f9b7140c1d4dc
Daniel Lovera <daanilo2497@gmail.com>
2025-05-13 00:57:30 -0300

    refactor: move frenet frames and swept transform logic into curve classes

--- src/utils/bezier_curve.ts
+++ src/utils/bezier_curve.ts
@@ -1,19 +1,37 @@
-import { Vector3, CurvePath, CubicBezierCurve3, QuadraticBezierCurve3 } from "three";
+import { Vector3, CurvePath, CubicBezierCurve3, QuadraticBezierCurve3, Matrix4 } from "three";
+import { ParametricGeometry } from "three/examples/jsm/Addons.js";
 
 interface CurveParams {
     points: number[][][];
+    segments?: number;
+    closed?: boolean;
 }
 
 export class BezierCurve {
-    private path;
+    private static readonly DEFAULT_CLOSED = true;
+    private static readonly DEFAULT_SEGMENTS = 200;
+    private segments;
+    private closed;
+    public path;
 
-    constructor(data: CurveParams) {
-        this.path = this.createPath(data);
+    constructor(optionalParams: CurveParams) {
+        const params = this.setParams(optionalParams);
+        this.segments = params.segments;
+        this.closed = params.closed;
+        this.path = this.createPath(params.points);
     }
 
-    private createPath(data: CurveParams): CurvePath<Vector3> {
+    private setParams(params: CurveParams) {
+        return {
+            points: params.points,
+            segments: params.segments ?? BezierCurve.DEFAULT_SEGMENTS,
+            closed: params.closed ?? BezierCurve.DEFAULT_CLOSED,
+        };
+    }
+
+    private createPath(points: number[][][]): CurvePath<Vector3> {
         const path = new CurvePath<Vector3>();
-        for (const curvePoints of data.points) {
+        for (const curvePoints of points) {
             if (curvePoints.length < 3 || curvePoints.length > 4) {
                 throw new Error(`Unexpected number of control points: ${curvePoints.length}. Expected 3 or 4.`);
             }
@@ -37,7 +55,25 @@ export class BezierCurve {
         return path;
     }
 
-    getPoints(segments = 200): Vector3[] {
-        return this.path.getPoints(segments);
+    parametricSwept(mappingFn: (u: number, v: number) => [number, number, number], slices: number) {
+        const { tangents, normals, binormals } = this.path.computeFrenetFrames(this.segments, this.closed);
+        const sweptFn = (u: number, v: number, target: Vector3) => {
+            const index = Math.floor(v * this.segments);
+            const tangent = tangents[index];
+            const normal = normals[index];
+            const binormal = binormals[index];
+            const curvePoint = this.path.getPointAt(v);
+            const sweptMatrix = new Matrix4()
+            sweptMatrix.makeBasis(normal, binormal, tangent);
+            sweptMatrix.setPosition(curvePoint);
+
+            const [x, y, z] = mappingFn(u, v);
+            const point = new Vector3(x, y, z);
+            point.applyMatrix4(sweptMatrix);
+            target.set(point.x, point.y, point.z);
+        };
+
+        const geometry = new ParametricGeometry(sweptFn, slices, this.segments);
+        return geometry;
     }
 }
--- src/utils/catmull_rom.ts
+++ src/utils/catmull_rom.ts
@@ -1,4 +1,4 @@
-import { CatmullRomCurve3, ExtrudeGeometry, ExtrudeGeometryOptions, Shape, Vector3 } from "three";
+import { CatmullRomCurve3, ExtrudeGeometry, ExtrudeGeometryOptions, Matrix4, Shape, Vector3 } from "three";
 import { ParametricGeometry } from "three/examples/jsm/Addons.js";
 
 interface CurveParams {
@@ -14,7 +14,7 @@ class CatmullRom {
     private static readonly DEFAULT_SEGMENTS = 200;
     private segments;
     private closed;
-    private curve;
+    public curve;
 
     constructor(optionalParams: CurveParams) {
         const params = this.setParams(optionalParams);
@@ -46,14 +46,26 @@ class CatmullRom {
         return geometry;
     }
 
-    parametricSwept(mappingFn: (u: number, v: number, target: Vector3) => void, slices: number) {
-        const geometry = new ParametricGeometry(mappingFn, slices, this.segments);
-        return geometry;
-    }
+    parametricSwept(mappingFn: (u: number, v: number) => [number, number, number], slices: number) {
+        const { tangents, normals, binormals } = this.curve.computeFrenetFrames(this.segments, this.closed);
+        const sweptFn = (u: number, v: number, target: Vector3) => {
+            const index = Math.floor(v * this.segments);
+            const tangent = tangents[index];
+            const normal = normals[index];
+            const binormal = binormals[index];
+            const curvePoint = this.curve.getPointAt(v);
+            const sweptMatrix = new Matrix4()
+            sweptMatrix.makeBasis(normal, binormal, tangent);
+            sweptMatrix.setPosition(curvePoint);
 
-    computeFrenetFrames() {
-        const vectors = this.curve.computeFrenetFrames(this.segments, this.closed);
-        return { ...vectors, segments: this.segments };
+            const [x, y, z] = mappingFn(u, v);
+            const point = new Vector3(x, y, z);
+            point.applyMatrix4(sweptMatrix);
+            target.set(point.x, point.y, point.z);
+        };
+
+        const geometry = new ParametricGeometry(sweptFn, slices, this.segments);
+        return geometry;
     }
 
     getPointAt(u: number) {
